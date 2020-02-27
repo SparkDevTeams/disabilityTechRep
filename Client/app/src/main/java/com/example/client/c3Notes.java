12998ec906fa114b46aaa4c13fe674967b43defc
JeffFitz9 <JeffFitz428@gmail.com>
2020-02-27 13:53:35 -0500

    first upload

---
+++ Client/app/src/main/java/com/example/client/c3Notes.java
@@ -0,0 +1,14 @@
+package com.example.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class c3Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c3_notes);
+    }
+}
