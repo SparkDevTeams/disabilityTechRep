77db97737edefbbdc39f25f4735df2f51d84f1a1
JeffFitz9 <JeffFitz428@gmail.com>
2020-02-27 13:57:36 -0500

    first upload

--- Client/app/src/main/java/com/example/client/c1Notes.java
+++ Client/app/src/main/java/com/example/client/c1Notes.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class C1Notes extends AppCompatActivity {
+public class c1Notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
